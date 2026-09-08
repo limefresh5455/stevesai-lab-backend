9ef64b7bb779ee6d84eb15ea29ab5b8305f2f48e
limefresh5455 <limefresh5455@gmail.com>
2026-09-08 18:16:40 +0530

    feat: Update FRONTEND_URL and CORS_ALLOWED_ORIGINS to use lists for better configuration

--- app/core/config.py
+++ app/core/config.py
@@ -9,8 +9,16 @@ class Settings(BaseSettings):
     ADMIN_INITIAL_PASSWORD: str = "password"
     JWT_SECRET_KEY: str = "secret"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
-    FRONTEND_URL: str = "http://localhost:3000"
-    CORS_ALLOWED_ORIGINS: str = "http://localhost:3000"
+    FRONTEND_URL:list = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://your-project-name.vercel.app",
+    ],
+    CORS_ALLOWED_ORIGINS: list = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://your-project-name.vercel.app",
+    ],
     SMTP_HOST: str = ""
     SMTP_PORT: int = 587
     SMTP_USER: str = ""
